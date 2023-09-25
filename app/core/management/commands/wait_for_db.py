ddb108a024d7c2da8646c81946f1a8629e02c9dd
leesh2578 <54782225+leesh2578@users.noreply.github.com>
2023-09-26 08:16:56 +0900

    Configured docker compose and check to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,7 +12,6 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
-
         db_up = False
         while db_up is False:
             try:
@@ -21,5 +20,4 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
         self.stdout.write(self.style.SUCCESS('Database available!'))
